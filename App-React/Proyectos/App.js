aa76e0ff18781f0c9308dc8b127f90432ceb045a
José Manuel Varela <josemanuelvarela36@gmail.com>
2023-03-21 22:31:51 -0600

    2.0

--- App-React/Proyectos/App.js
+++ App-React/Proyectos/App.js
@@ -8,6 +8,12 @@ import Eliminar_Proyectos from "./Proyectos_Modulo/Eliminar_Proyectos";
 import Menu_Proyectos from "./Proyectos_Modulo/Menu_Proyectos";
 import Modificar_Proyectos from "./Proyectos_Modulo/Modificar_Proyectos";
 import Registrar_Proyectos from "./Proyectos_Modulo/Registrar_Proyectos";
+import Consultar_Revision from "./Revisiones/Consultar_Revisiones";
+import Eliminar_Revision from "./Revisiones/Eliminar_Revisiones";
+import Menu_Revisiones from "./Revisiones/Menu_Revisiones";
+import Modificar_Revision from "./Revisiones/Modificar_Revisiones";
+import Registrar_Revision from "./Revisiones/Registrar_Revisiones";
+import Menu_Entregables from "./Ui_Entrgables/Ent_Menu";
 
 
 
@@ -24,6 +30,18 @@ export default function App() {
         <Stack.Screen name="ConsultarProyecto" component={Consultar_Proyectos} />
         <Stack.Screen name="ModificarProyecto" component={Modificar_Proyectos} />
 
+        <Stack.Screen name="MenuRevisiones" component={Menu_Revisiones} />
+        <Stack.Screen name="EliminarRevisiones" component={Eliminar_Revision} />
+        <Stack.Screen name="ConsultarRevisiones" component={Consultar_Revision} />
+        <Stack.Screen name="AgregarRevisiones" component={Modificar_Revision} />
+        <Stack.Screen name="ModificarRevisiones" component={Registrar_Revision} />
+
+
+        <Stack.Screen name="MenuEntregables" component={Menu_Entregables} />
+        <Stack.Screen name="Ent_Agregar" component={Ennt} />
+        <Stack.Screen name="Ent_Consultar" component={Consultar_Revision} />
+        <Stack.Screen name="Ent_Eliminar" component={Modificar_Revision} />
+        <Stack.Screen name="Ent_Modificar" component={Registrar_Revision} />
       </Stack.Navigator>
     </NavigationContainer>
   );
