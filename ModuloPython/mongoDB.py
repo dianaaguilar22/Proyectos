183adaa5a46b136f3257b7567d934a7807d0b2d2
José Manuel Varela <josemanuelvarela36@gmail.com>
2023-05-15 20:37:06 -0600

    Correcciones

--- ModuloPython/mongoDB.py
+++ ModuloPython/mongoDB.py
@@ -1,8 +1,13 @@
 from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+
 
 class Conexion():
 
     def __init__(self):
-        self.cliente = MongoClient()
-        self.bd = self.cliente.titulaTec
-        self.col=self.bd.proyectos()
+        self.cliente = MongoClient("mongodb+srv://admin:admin@proyectos.bw10bli.mongodb.net/?retryWrites=true&w=majority")
+        self.bd = self.cliente.Proyectos
+        self.proyectos = self.bd.proyecto
+        self.entregables = self.bd.entregables
+        self.revisiones = self.bd.revisiones
+
--- ModuloPython/rutaBluePrints.py
+++ ModuloPython/rutaBluePrints.py
@@ -1,15 +1,16 @@
-from flask import blueprints,request
-
+from flask import blueprints, request
 from ProyectosRest.ModuloPython.ModuloProyectos.Proyectos import Proyecto
+from mongoDB import Conexion
 
 proyectosBP = blueprints.Blueprint("Proyectos", __name__)
-
 
 
 
 @proyectosBP.route("/proyecto/revisiones")
 def mostrarRev():
     return "Hola en Revsiones"
+
+
 # Aquí va las funciones
 
 @proyectosBP.route("/proyecto/entregables")
@@ -20,3 +21,9 @@ def mostrarEnt():
 @proyectosBP.route("/proyecto/proyectos", methods=['GET'])
 def mostrar():
     return Proyecto.proyectos()
+
+
+@proyectosBP.route("/proyecto/proyectos", methods=['POST'])
+def insertar():
+    datos = request.get_json()
+    return
